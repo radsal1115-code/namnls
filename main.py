b1b6fc00c7f581b13b3233baf6a9702dd9da78b0
radsal1115-code <radsal1115@utb.partille.se>
2026-05-22 13:07:00 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_sound_quiet():
+    pass
+input.on_sound(DetectedSound.QUIET, on_sound_quiet)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onSound(DetectedSound.Quiet, function on_sound_quiet() {
+    
+})
+basic.forever(function on_forever() {
+    
+})
